e38eaf8234c2370a5795b4b00bf62086c345965a
Alexandr <44715875+lunst97@users.noreply.github.com>
2021-07-13 17:50:39 +0300

    Update cart.rb

--- lib/cart.rb
+++ lib/cart.rb
@@ -3,7 +3,6 @@ class Cart
     @products = products
   end
 
-  # складывает все в корзину
   def add_product(chosen_product)
     @products << chosen_product
   end
@@ -12,7 +11,6 @@ class Cart
     @products.sum { |product| product.price }
   end
 
-  # выводит всю корзину
   def to_s
     @products.join("\n")
   end
