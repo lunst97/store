3bd1c2dfd07c8c1f80649214f3a73b41425454a4
Alexandr <44715875+lunst97@users.noreply.github.com>
2021-07-13 17:51:36 +0300

    Update product.rb

--- lib/product.rb
+++ lib/product.rb
@@ -1,5 +1,4 @@
 class Product
-  # создает аксессор на ввод и вывод
   attr_accessor :amount, :price
 
   def initialize(params)
@@ -7,18 +6,15 @@ class Product
     @price = params[:price]
   end
 
-  # выводит цену и кол-во во всех "детях" класса
   def to_s
     " #{@price} руб. (осталось #{@amount})"
   end
 
-  # дает возможность обновить данные
   def update(params)
     @amount = params[:amount] if params[:amount]
     @price = params[:price] if params[:price]
   end
 
-  # выводит ошибку если находит файл
   def self.from_file(file_path)
     raise NotImplementedError
   end
